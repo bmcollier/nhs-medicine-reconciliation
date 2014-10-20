f4255dadd13b12ef8bb77e4010f12ba9d9ae9a51
Mike Leonard <mike.leonard@fivium.co.uk>
2014-10-20 20:33:32 +0100

    Use local jquery for django-debug-toolbar as its included anyway and this speeds things up.

--- medchecker/medchecker/settings/local.py
+++ medchecker/medchecker/settings/local.py
@@ -60,6 +60,11 @@ MIDDLEWARE_CLASSES += (
 
 DEBUG_TOOLBAR_PATCH_SETTINGS = False
 
+DEBUG_TOOLBAR_CONFIG = {
+    'JQUERY_URL': '/static/js/jquery.min.js',
+}
+
 # http://django-debug-toolbar.readthedocs.org/en/latest/installation.html
 INTERNAL_IPS = ('127.0.0.1',)
 ########## END TOOLBAR CONFIGURATION
+
