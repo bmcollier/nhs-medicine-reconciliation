00c5a46cbbf598bafcb8ac6915422b3736223bc1
Mike Leonard <mike.leonard@fivium.co.uk>
2014-10-23 14:45:28 +0100

    Updated patient menu following designs from Natasha.

--- medchecker/static/js/medchecker.js
+++ medchecker/static/js/medchecker.js
@@ -1,1 +1,13 @@
-/* Project specific Javascript goes here. */
+$('.top-bar img:first').click(function () {
+  var openClose = $(this).next().next();
+  var patientLabel = $('#patient-name').prev();
+  if (openClose.hasClass('hide')) {
+    openClose.removeClass('hide');
+    $("#patient-name a").css('color', 'rgb(218,220,219)');
+    patientLabel.css('color', 'rgb(218,220,219)');
+  } else {
+    openClose.addClass('hide');
+    patientLabel.css('color', 'rgb(65, 79, 90)');
+    $('#patient-name a').css('color', 'rgb(0, 140, 186)');
+  }
+});
