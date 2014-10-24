118c077a0695ded9fee754ee2e59b525f4624093
Mike Leonard <mike.leonard@fivium.co.uk>
2014-10-24 09:46:13 +0100

    Added recent design changes from Tasha, specifically the reworked header that includes the patient details.

--- medchecker/static/js/medchecker.js
+++ medchecker/static/js/medchecker.js
@@ -1,5 +1,5 @@
-$('.top-bar img:first').click(function () {
-  var openClose = $(this).next().next();
+$('#patient-menu-icon').click(function () {
+  var openClose = $('#patient-menu');
   var patientLabel = $('#patient-name').prev();
   if (openClose.hasClass('hide')) {
     openClose.removeClass('hide');
@@ -9,5 +9,30 @@ $('.top-bar img:first').click(function () {
     openClose.addClass('hide');
     patientLabel.css('color', 'rgb(65, 79, 90)');
     $('#patient-name a').css('color', 'rgb(0, 140, 186)');
+  }
+});
+
+$('#user-menu-icon').click(function () {
+  var openClose = $('#user-menu');
+  var currentUserNameLabel = $('#current-user-name').prev();
+  if (openClose.hasClass('hide')) {
+    openClose.removeClass('hide');
+    $("#current-user-name").css('color', 'rgb(218,220,219)');
+    currentUserNameLabel.css('color', 'rgb(218,220,219)');
+  }
+  else {
+    openClose.addClass('hide');
+    currentUserNameLabel.css('color', 'rgb(65, 79, 90)');
+    $('#current-user-name').css('color', 'rgb(65, 79, 90)');
+  }
+});
+
+$('#moreInfoTab').click(function () {
+  var openClose = $('#moreInfo');
+  if (openClose.hasClass('hide')) {
+    openClose.removeClass('hide');
+  }
+  else {
+    openClose.addClass('hide');
   }
 });
