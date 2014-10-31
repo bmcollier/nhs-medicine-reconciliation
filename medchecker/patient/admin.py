a6ef4de416103f5756e174af1060c84773ad93c8
bmcollier <bmcollier@gmail.com>
2014-10-31 12:16:19 +0000

    Adding GP to admin page.

--- medchecker/patient/admin.py
+++ medchecker/patient/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from patient.models import Patient, PatientMedication, GPMedication
+from patient.models import Patient, PatientMedication, GPMedication, GeneralPractitioner
 
 class PatientAdmin(admin.ModelAdmin):
     search_fields = ['first_name', 'last_name']
@@ -8,3 +8,4 @@ class PatientAdmin(admin.ModelAdmin):
 admin.site.register(Patient, PatientAdmin)
 admin.site.register(PatientMedication)
 admin.site.register(GPMedication)
+admin.site.register(GeneralPractitioner)
