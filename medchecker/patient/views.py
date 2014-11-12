03aabd647b4a5791d1c6779e66848e1cd47b0974
bmcollier <bmcollier@gmail.com>
2014-11-12 11:00:23 +0000

    New data model. Branching.

--- medchecker/patient/views.py
+++ medchecker/patient/views.py
@@ -4,8 +4,8 @@ from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 
-from patient.models import Patient, PatientMedication, GPMedication
-from patient.forms import PatientMedicationModelForm
+from patient.models import Patient, PatientMedication, GPMedication, Medication
+from patient.forms import PatientMedicationModelForm, BarcodeMedicationForm
 
 @login_required
 def search(request):
@@ -27,7 +27,7 @@ def add(request):
 def detail(request, patient_id):
     patient = get_object_or_404(Patient.objects.select_related('general_practitioner'), id=patient_id)
 
-    medications = PatientMedication.objects.select_related('virtual_medicinal_product', 'virtual_medicinal_product__vtmid').filter(patient=patient).exclude(status='2 DELETED').order_by('status', 'virtual_medicinal_product__nm')
+    medications = Medication.objects.select_related('virtual_medicinal_product', 'virtual_medicinal_product__vtmid').filter(patient=patient).exclude(status='2 DELETED').order_by('status', 'virtual_medicinal_product__nm')
     # verified_medications = PatientMedication.objects.select_related('virtual_medicinal_product').filter(patient=patient, status='VERIFIED')
 
     return render_to_response(
@@ -64,11 +64,37 @@ def add_medicine(request, patient_id):
         )
 
 @login_required
-def edit_medicine(request, patient_id):
+def scan_medicine(request, patient_id):
     patient = get_object_or_404(Patient.objects.select_related('general_practitioner'), id=patient_id)
 
     if request.method == 'POST':
-        patient_medication_form = PatientMedicationModelForm(request.POST)
+        barcode_medication_form = BarcodeMedicationForm(request.POST)
+        if barcode_medication_form.is_valid():
+            barcode_medication = barcode_medication_form.save(commit=False)
+            barcode_medication.patient = patient
+
+            barcode_medication.save()
+
+            return HttpResponseRedirect(
+                reverse('patient_detail', kwargs={'patient_id': patient.id,})
+                )
+        else:
+            print 'not valid'
+
+    else:
+        barcode_medication_form = BarcodeMedicationForm() # An unbound form
+
+    return render_to_response(
+        'barcode_medication_form.html',
+        context_instance=RequestContext(request, {'patient': patient, 'barcode_medication_form': barcode_medication_form})
+        )
+
+@login_required
+def edit_medicine(request, patient_id, medicine_id=None):
+    patient = get_object_or_404(Patient.objects.select_related('general_practitioner'), id=patient_id)
+
+    if request.method == 'POST':
+        patient_medication_form = MedicationModelForm(request.POST)
         if patient_medication_form.is_valid():
             patient_medication = patient_medication_form.save(commit=False)
             patient_medication.patient = patient
@@ -82,56 +108,42 @@ def edit_medicine(request, patient_id):
             print 'not valid'
 
     else:
-        patient_medication_form = PatientMedicationModelForm() # An unbound form
-
+        if medicine_id:
+            med_ref = Medication.objects.get(id=medicine_id)
+            patient_medication_form = PatientMedicationModelForm(instance=med_ref) # An unbound form
+        else:
+            patient_medication_form = PatientMedicationModelForm()
+        
     return render_to_response(
-        'edit_medicine.html',
+        'edit_medicine_2.html',
         context_instance=RequestContext(request, {'patient': patient, 'patient_medication_form': patient_medication_form})
         )
 
 
 @login_required
-def reconcile_medicine(request, patient_id):
+def reconcile_medicines(request, patient_id):
     patient = get_object_or_404(Patient.objects.select_related('general_practitioner'), id=patient_id)
 
-    history_medications = PatientMedication.objects.select_related('virtual_medicinal_product', 'virtual_medicinal_product__vtmid').filter(patient=patient).exclude(status='2 DELETED')
-    gp_medications = GPMedication.objects.select_related('virtual_medicinal_product', 'virtual_medicinal_product__vtmid').filter(patient=patient).exclude(status='2 DELETED')
-
-    medications_dict = {}
-    for medication in history_medications:
-        if medications_dict.get(str(medication.virtual_medicinal_product.vtmid.vtmid)):
-            current_dict = medications_dict[str(medication.virtual_medicinal_product.vtmid.vtmid)]
-            new_dict = dict(current_dict.items() + {'history': medication,}.items())
-            medications_dict[str(medication.virtual_medicinal_product.vtmid.vtmid)] = new_dict
+    medications = Medication.objects.select_related('virtual_medicinal_product', 'virtual_medicinal_product__vtmid').filter(patient=patient).exclude(status='2 DELETED')
+    categories = []
+    
+    for medication in medications:
+        if medication.virtual_medicinal_product.vtmid.nm <> '':
+            medication.category = medication.virtual_medicinal_product.vtmid.nm
+            medication.display_name = medication.virtual_medicinal_product.nm
         else:
-            medications_dict[str(medication.virtual_medicinal_product.vtmid.vtmid)] = {
-                'history': medication,
-                'medinfo': {
-                    'vpid': medication.virtual_medicinal_product.vpid, 'nm': medication.virtual_medicinal_product.nm
-                    }
-                }
-
-    for medication in gp_medications:
-        if medications_dict.get(str(medication.virtual_medicinal_product.vtmid.vtmid)):
-            current_dict = medications_dict[str(medication.virtual_medicinal_product.vtmid.vtmid)]
-            new_dict = dict(current_dict.items() + {'gp': medication,}.items())
-            medications_dict[str(medication.virtual_medicinal_product.vtmid.vtmid)] = new_dict
-        else:
-            medications_dict[str(medication.virtual_medicinal_product.vtmid.vtmid)] = {
-                'gp': medication,
-                'medinfo': {
-                    'vpid': medication.virtual_medicinal_product.vpid, 'nm': medication.virtual_medicinal_product.nm
-                    }
-                }
-
-    medications = []
-    for k, v in medications_dict.iteritems():
-        medications += [v,]
-
+            medication.category = medication.virtual_therapeutic_moeity.nm
+            medication.display_name = medication.virtual_medicinal_product.nm
+        categories.append(medication.category)
+        
+    categories = set(categories)
+    categories = list(categories)
+    medications.order_by('category')
+    
     return render_to_response(
-        'reconcile_medicine.html',
+        'reconcile_medicines.html',
         context_instance=RequestContext(request,
-            {'patient': patient, 'medications': medications}
+            {'patient': patient, 'medications': medications, 'categories': categories }
             )
         )
 
